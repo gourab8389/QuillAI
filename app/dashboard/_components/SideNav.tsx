f929758a1f87e837b46fdc00c22f0adad866450a
gourab8389 <deyg6988@gmail.com>
2024-06-26 00:47:56 +0530

    header

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,8 +1,16 @@
+"use client"
 import { MenuList } from '@/constants'
 import Image from 'next/image'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 const SideNav = () => {
+
+    const path = usePathname();
+    useEffect(()=>{
+        console.log(path)
+    })
+
     return (
         <div className='h-screen p-5 shadow-sm border'>
             <div className="flex justify-center">
@@ -10,12 +18,13 @@ const SideNav = () => {
                 <Image src={'/logo.svg'} alt='logo' width={60} height={70} />
                 <Image src={'/logo2.svg'} alt='logo2' width={70} height={80}/>
             </div>
-            <div className="mt-10">
+            <hr className='my-6 border' />
+            <div className="mt-3">
 
                 {MenuList.map((menu, index) => (
-                    <div className="flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer">
-                        <menu.icon />
-                        <h2>{menu.name}</h2>
+                    <div className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer items-center ${path==menu.path&&'bg-primary text-white'}`}>
+                        <menu.icon className='w-6 h-6'/>
+                        <h2 className='text-lg'>{menu.name}</h2>
                     </div>
                 ))}
             </div>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SideNav from './_components/SideNav'
+import Header from './_components/Header'
 
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     return (
@@ -8,6 +9,7 @@ const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
                 <SideNav />
             </div>
             <div className="md:ml-64">
+                <Header/>
                 {children}
             </div>
         </div>
