5fa909dee15f28d44ff7113706fb97707e8c8c12
gourab8389 <deyg6988@gmail.com>
2024-06-25 16:01:21 +0530

    clerk add

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
       <h1>Home</h1>
+      
+        <UserButton/>
       <Button>
         nepo
       </Button>
