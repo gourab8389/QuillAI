655dd9b2bbea71e099f9010d1df5402a179526eb
gourab8389 <deyg6988@gmail.com>
2024-06-26 21:53:49 +0530

    edit page

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import FormSection from '../_components/FormSection'
+import OutputSection from '../_components/OutputSection'
+import { TEMPLATE } from '../../_components/TemplateListSection'
+import { Templates } from '@/app/(data)/Templates'
 
 interface PROPS {
     params:{
@@ -8,14 +12,16 @@ interface PROPS {
 
 const CreateNewContent = (props:PROPS) => {
 
-    
+    const selectedTemplate: TEMPLATE|undefined=Templates?.find((item)=>item.slug==props.params['template-slug']);
 
   return (
-    <div>
+    <div className='grid grid-cols-2 md:grid-cols-2 gap-10 p-5'>
       {/* FormSection */}
+      <FormSection selectedTemplate={selectedTemplate}/>
 
       {/* OutputSection */}
-      
+      <OutputSection/>
+
     </div>
   )
 }
--- app/dashboard/content/_components/FormSection.tsx
+++ app/dashboard/content/_components/FormSection.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { TEMPLATE } from '../../_components/TemplateListSection'
+import Image from 'next/image'
 
-function FormSection() {
+interface PROPS {
+    selectedTemplate?:TEMPLATE
+}
+
+function FormSection({selectedTemplate}:PROPS) {
   return (
-    <div>
-      form section
+    <div className='p-5 shadow-md border rounded-lg'>
+        {/* @ts-ignore*/}
+        <Image src={selectedTemplate?.icon} alt='icon' width={70} height={70}/>
+      <h2 className='font-bold text-2xl mb-2 text-primary'>{selectedTemplate?.name}</h2>
+      <p className='text-gray-500 text-sm'>{selectedTemplate?.desc}</p>
+      <form action="">
+        {selectedTemplate?.form?.map((item, index)=>(
+            <div className="">
+                <label htmlFor="">
+                    {item.label}
+                </label>
+            </div>
+        ))}
+      </form>
     </div>
   )
 }
