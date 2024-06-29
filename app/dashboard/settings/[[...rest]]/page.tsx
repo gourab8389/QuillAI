9bfb0bf1f4cdab95b2ffce28b9e2f785427c7cec
gourab8389 <deyg6988@gmail.com>
2024-06-29 20:09:41 +0530

    fixing

--- app/dashboard/settings/[[...rest]]/page.tsx
+++ app/dashboard/settings/[[...rest]]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Settings() {
   return (
-    <div className='flex items-center justify-center h-full'>
+    <div className='flex items-center justify-center h-full ml-20 w-50% mt-5'>
       <UserProfile/>
     </div>
   )
