4c4aaee1626a147e2bad77b9acd56732a24ad90a
gourab8389 <deyg6988@gmail.com>
2024-06-30 11:32:07 +0530

    search bar hidden

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='p-5 shadow-sm border-b-2  bg-white flex justify-between items-center'>
-      <div className="flex gap-2 items-center border rounded-md max-w-lg bg-white sm:hidden">
-        <Search/>
-      <input type="text" placeholder='Search...' className='outline-none' />
+    <div className='p-5 shadow-sm border-b-2 bg-white flex justify-between items-center'>
+      <div className="hidden md:flex gap-2 items-center border rounded-md max-w-lg bg-white">
+        <Search />
+        <input type="text" placeholder='Search...' className='outline-none' />
       </div>
       <div className="flex gap-5 items-center">
         <h2 className='bg-primary p-1 rounded-full text-xs text-white px-2'>
           🔥Join Membership just for ₹300/month
         </h2>
-        <UserButton/>
+        <UserButton />
       </div>
     </div>
   )
